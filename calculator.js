dc208c6c6c627cef7b80b32ed76808db29d4cdcd
MahdiMohammadiME <74717890+MahdiMohammadiME@users.noreply.github.com>
2025-12-15 20:04:52 +0330

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -3,9 +3,29 @@ function calculate() {
     let headThk = parseFloat(document.getElementById("headThk").value) || 0;
     let shellId = parseFloat(document.getElementById("shellId").value) || 0;
 
-    let headId = shellThk * 2;
-    let headOd = headThk * 3;
-    let nsfLength = shellId * 4;
+    let count = 0;
+    let Y = 0;
+    
+    do{
+        let Y_old = Y;
+        
+        let shellNAD = shellThk + shellId || 0;
+        let shellOD = 2*shellThk + shellId || 0;
+    
+        let headNAR = shellNAD/2 - count;
+        let headIR = headNAR - headThk/2;
+    
+        let Y = math.sqrt(headIR**2 - (shellThk/2 - shellNAD/2)**2);
+        let headOR = headNAR + headThk/2;
+        let deltaX = shellThk/2 - (headOR - shellNAD/2);
+    
+        count++;
+
+    }while(Y >= 3*deltaX);
+
+    let headId = headIR*2;
+    let headOd = headOR*2;
+    let nsfLength = Y_old;
 
     document.getElementById("headId").textContent = headId;
     document.getElementById("headOd").textContent = headOd;
